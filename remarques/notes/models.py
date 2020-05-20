6ab2d911e635f5a32276f8031c78726e05ae82ab
su-gu <subhasishgupta970@gmail.com>
2020-05-21 01:54:21 +0530

    :sparkles: Add data base Add database and update setting of django database

--- remarques/notes/models.py
+++ remarques/notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 # Create your models here.
+class notepad(models.Model):
+    url = models.CharField(max_length=100)
+    notes = JSONField()
+
+    def __str__(self):
+        return self.url
--- remarques/scripts/generator.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-url_length = 10
-
-
-def generateRandomURL():
-    letters = string.ascii_letters
-    digits = string.digits
-    return ''.join(random.sample(letters + digits, url_length))
